20f885c35abecf0ce1f19af7730b64cc78a5a9aa
Fiqri Syah Redha <wibulo27@gmail.com>
2020-06-28 06:08:53 +0700

    Add length converter page

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,31 @@
-const elements = {
+elements = {
     content: document.querySelector('#content'),
+    sidenav: document.querySelector('.sidenav'),
+    sidenavLink: document.querySelectorAll('.sidenav a'),
+    buttonCard: document.querySelectorAll('#home-content .card'),
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    M.Sidenav.init(elems);
 
-    loadPage('home');
+    elements.sidenavLink.forEach((elm) => {
+        elm.addEventListener('click', (event) => {
+            M.Sidenav.getInstance(elements.sidenav).close();
+            page = event.target.getAttribute('href').substr(1);
+            loadPage(page);
+        });
+    });
 });
 
 const loadPage = (page) => {
+    if (page === '') page = 'home';
+
     fetch(`./pages/${page}.html`)
         .then((response) => response.text())
         .then((html) => {
             elements.content.innerHTML = html;
         });
 };
+
+loadPage(window.location.hash.substr(1));
